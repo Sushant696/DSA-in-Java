d0ae7da7263385d3fdc2c8d4d5daf0d1a00051a5
sushant <sushantprasaii7@gmail.com>
2024-11-29 10:35:21 +0545

    Feat: Reverse a array using stack

---
+++ Stacks/ReverseArray.java
@@ -0,0 +1,75 @@
+package Stacks;
+
+/**
+ * Questions :- Reverse a array using stack
+ * ReverseArray
+ */
+
+public class ReverseArray {
+
+  static int[] reverseArray(int arr[]) {
+    int[] revArr = new int[5];
+    Stacks stk = new Stacks(6);
+    for (int i : arr) {
+      stk.push(i);
+    }
+    for (int i = 0; i < revArr.length; i++) {
+      int popItem = stk.pop();
+      revArr[i] = popItem;
+    }
+    //
+
+    return revArr;
+  }
+
+  public static void main(String[] args) {
+    int arr[] = { 1, 2, 3, 4, 5 };
+    int revArr[] = reverseArray(arr);
+    System.out.print("Reverse array :");
+    for (int i : revArr) {
+      System.out.print(i);
+    }
+  }
+}
+
+class Stacks {
+  int size;
+  int stk[];
+  int top = -1;
+
+  // making stack here to use to reverse the array
+  Stacks(int size) {
+    this.size = size;
+    stk = new int[size];
+  }
+
+  boolean isEmpty() {
+    return top == -1;
+  }
+
+  boolean isFull() {
+    return top == size - 1;
+  }
+
+  int peek() {
+    System.out.println(stk[top]);
+    return stk[top];
+  }
+
+  int pop() {
+    if (isEmpty()) {
+      System.out.println("Stack is empty, no item to pop");
+      return -1;
+    } else {
+      return stk[top--];
+    }
+  }
+
+  void push(int data) {
+    if (isFull()) {
+      System.out.println("Stack overflow, stack is full");
+    } else {
+      stk[++top] = data;
+    }
+  }
+}
---
+++ linkedList/linkedList.java
@@ -0,0 +1,67 @@
+package linkedList;
+
+import java.util.LinkedList;
+
+/**
+ * linkedList
+ */
+public class linkedList {
+
+  public static class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+      this.data = data;
+      this.next = null;
+    }
+  }
+
+  Node head = null;
+  Node tail = null;
+
+  void addNode(int data) {
+    Node newNode = new Node(data);
+
+    if (head == null) {
+      head = newNode;
+      tail = newNode;
+    } else {
+      // Node current = head;
+      // while (current.next != null) {
+      // current = current.next;
+      // }
+      // current.next = newNode;
+      //
+      tail.next = newNode;
+      tail = newNode;
+    }
+  }
+
+  void addHeadFirst(int data) {
+    Node newNode = new Node(data);
+    if (head == null) {
+      head = tail = newNode;
+    } else {
+      newNode.next = head;
+      head = newNode;
+    }
+  }
+
+  void printList() {
+    Node current = head;
+    while (current != null) {
+      System.out.println(current.data);
+      current = current.next;
+    }
+  }
+
+  public static void main(String[] args) {
+    linkedList list = new linkedList();
+    list.addNode(10);
+    list.addNode(20);
+    list.addNode(30);
+    list.printList();
+  }
+
+}
